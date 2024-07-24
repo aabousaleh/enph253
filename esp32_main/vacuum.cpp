ecab0958c3df1859a75f7851ee14b40de90a0076
Pawan Daliaho <120357799+pawandaliaho@users.noreply.github.com>
2024-07-24 11:11:46 -0700

    Added current sense averaging

--- esp32_main/vacuum.cpp
+++ esp32_main/vacuum.cpp
@@ -30,9 +30,43 @@ bool Vacuum::objSecured() {
   //TODO: NEED TO BE CHANGED EXPERIMENTALLY
   int LOWER_THRESHOLD = 400;
   int HIGHER_THRESHOLD = 600;
-  int currentReading = analogRead(sensPin);
 
-  if (currentReading < LOWER_THRESHOLD || currentReading > HIGHER_THRESHOLD) pickedUp = true;
+  //TODO: MIGHT NEED TO CHANGE THIS
+  const int NUM_READINGS = 50;
+  int currentReadings[NUM_READINGS];
+  int index = 0;
+  int total = 0;
+  int avg = 0;
+
+  //intializes all values in the array to 0
+  for (int i = 0; i < NUM_READINGS; i++) {
+    currentReadings[i] = 0;
+  }
+  
+  const uint32_t DESIRED_PERIOD = 1000;
+  uint32_t tStart = millis();
+  uint32_t tNow = millis();
+  bool startCheck = false;
+
+  while (tNow - tStart <= DESIRED_PERIOD) {
+    total -= currentReadings[index];
+    currentReadings[index] = analogRead(sensPin);
+    total += currentReadings[index];
+
+    if (index < NUM_READINGS) i++;
+    else {
+      i = 0;
+      startCheck = true;
+    }
+
+    avg = total / NUM_READINGS;
+
+    if (startCheck) {
+      if (avg < LOWER_THRESHOLD || avg > HIGHER_THRESHOLD) pickedUp = true;
+    }
+
+    tNow = millis();
+  }
 
   return pickedUp;
 }
