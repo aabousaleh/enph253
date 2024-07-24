b08f61d748316e0e8608c4a686fbbe7d45acbf9c
Pawan Daliaho <120357799+pawandaliaho@users.noreply.github.com>
2024-07-24 15:37:03 -0700

    Added comments

--- esp32_main/vacuum.h
+++ esp32_main/vacuum.h
@@ -11,7 +11,18 @@ class Vacuum {
   int valvePin; //control pin (digital out) for solenoid valve
   Vacuum(int _ctrlPin, int _sensPin, int _valvePin);
 
+  /**
+ * Turns vacuum end effector on or off (picks up or drops object)
+ * 
+ * Param: 'true' to suction object, 'false' to drop the object
+ */
   void setSucc(bool succ);
+
+  /**
+ * Confirms if an object has been picked up.
+ * 
+ * Return: 'true' if object was picked up, 'false' otherwise
+ */
   bool objSecured();
 };
 
